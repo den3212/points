8b172b82773e39e10d077d3a592c5dec7a1a049c
deniskovalev <deniskovalev@192.168.0.103>
2015-09-09 01:24:52 +0300

    1

---
+++ src/main/java/ru/sunflower/points/Point.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sunflower.points;
+
+/**
+ *
+ * @author den
+ */
+public class Point {
+  private String name;
+  private String address;
+  private Double latitude;
+  private Double longitude;
+
+  public Point(){
+    
+  }
+  
+  public Point(String name, String address, Double latitude, Double longitude){
+    this.name = name;
+    this.address = address;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+  
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * @return the address
+   */
+  public String getAddress() {
+    return address;
+  }
+
+  /**
+   * @param address the address to set
+   */
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  /**
+   * @return the latitude
+   */
+  public Double getLatitude() {
+    return latitude;
+  }
+
+  /**
+   * @param latitude the latitude to set
+   */
+  public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+  }
+
+  /**
+   * @return the longitude
+   */
+  public Double getLongitude() {
+    return longitude;
+  }
+
+  /**
+   * @param longitude the longitude to set
+   */
+  public void setLongitude(Double longitude) {
+    this.longitude = longitude;
+  }
+  
+}
